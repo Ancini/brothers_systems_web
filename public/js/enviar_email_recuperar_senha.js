81d48f0a6854d274d0845478c38fc5c459c52c93
Gabriel Eduardo Ancini <131417388+Ancini@users.noreply.github.com>
2026-04-13 18:46:51 -0300

    atualizado1846

---
+++ public/js/enviar_email_recuperar_senha.js
@@ -0,0 +1,25 @@
+const supabaseClient = supabase.createClient(
+  "https://hnaapsbkrokrkmnzayyr.supabase.co",
+  "sb_publishable_AaxUlPsbivnRIu2_iu3Epg_nzr8w-3u"
+);
+
+async function recuperarSenha(event) {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+
+    try {
+
+        const { error } = await supabaseClient.auth.resetPasswordForEmail(email, {
+            redirectTo: "http://localhost:5500/nova-senha.html"
+        });
+
+        if (error) throw error;
+
+        alert("Se o email existir, você receberá um link para redefinir a senha.");
+
+    } catch (erro) {
+        console.error(erro);
+        alert("Erro ao enviar email");
+    }
+}
